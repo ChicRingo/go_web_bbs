30be3122760bd6df2065825fdede0e83a0f37c0a
miodawn <778942292@qq.com>
2020-09-11 22:07:56 +0800

    update

--- settings/settings.go
+++ settings/settings.go
@@ -65,22 +65,24 @@ func Init() (err error) {
 	//因为字节流中没有文件扩展名，所以支持的扩展名是“ json”，“ toml”，“ yaml”，“ yml”，“ properties”，“ props”，“ prop”，“ env”，“ dotenv”
 	//viper.SetConfigType("yaml") // 指定配置文件(专用于从远程获取配置信息时指定配置文件类型的)
 
-	err = viper.ReadInConfig() // 读取配置信息
-	if err != nil {            // 读取配置信息失败
+	// 读取配置信息
+	if err = viper.ReadInConfig(); err != nil {
 		// 读取配置文件失败
 		fmt.Printf("viper.ReadInConfig() failed, err:%v\n", err)
 		return
 	}
 	// 把读取到的配置信息反序列化到 Conf 变量中
-	if err := viper.Unmarshal(Conf); err != nil {
+	if err = viper.Unmarshal(Conf); err != nil {
 		fmt.Printf("viper.Unmarshal() failed, err:%v\n", err)
+		return
 	}
 	// 监控配置文件变化
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("配置文件修改了...")
-		if err := viper.Unmarshal(Conf); err != nil {
+		if err = viper.Unmarshal(Conf); err != nil {
 			fmt.Printf("viper.Unmarshal() failed, err:%v\n", err)
+			return
 		}
 	})
 	return
